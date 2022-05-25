545428dd91f99b19c41874c11724700750b9c46c
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-25 13:37:56 +0200

    Add : Observable<Column> and column: Column type

--- src/app/services/column.service.ts
+++ src/app/services/column.service.ts
@@ -1,28 +1,30 @@
+import { Observable } from 'rxjs';
 import { RequestService } from './request.service';
 import { Injectable } from '@angular/core';
+import { Column } from '../models/column';
 
 @Injectable()
 export class ColumnService {
 
 constructor(private http: RequestService) { }
 
-public getAllColumns(boardId: string) {
+public getAllColumns(boardId: string): Observable<Column[]> {
   return this.http.getRequest('/boards/' + boardId + '/columns')
 }
 
-public createColumn(boardId: string, column: any) {
+public createColumn(boardId: string, column: Column): Observable<Column> {
   return this.http.postRequest('/boards/' + boardId + '/columns', column)
 }
 
-public getColumn(boardId: string, columnId: string) {
+public getColumn(boardId: string, columnId: string): Observable<Column> {
   return this.http.getRequest('/boards/' + boardId + '/columns/' + columnId)
 }
 
-public deleteColumn(boardId: string, columnId: string) {
+public deleteColumn(boardId: string, columnId: string): Observable<Column> {
   return this.http.deleteRequest('/boards/' + boardId + '/columns/' + columnId)
 }
 
-public updateColumn(boardId: string, columnId: string, column: any) {
+public updateColumn(boardId: string, columnId: string, column: Column): Observable<Column> {
   return this.http.putRequest('/boards/' + boardId + '/columns/' + columnId, column)
 }
 }
