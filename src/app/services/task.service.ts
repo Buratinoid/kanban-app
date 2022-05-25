2f023d3e26ca8d1dc0455ea32babf76e6d83e2eb
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-25 12:41:22 +0200

    Add createTask() method

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -9,4 +9,8 @@ constructor(private http: RequestService) { }
 public getAllTasks(boardId: string, columnId: string) {
   return this.http.getRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks')
 }
+
+public createTask(boardId: string, columnId: string, task: any) {
+  return this.http.postRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks', task)
+}
 }
