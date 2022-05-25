98163797e2abe396857a7172fda19961c7560761
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-25 13:46:11 +0200

    Add Observable<Task> and task: Task type

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,28 +1,30 @@
-import { RequestService } from './request.service';
 import { Injectable } from '@angular/core';
+import { RequestService } from './request.service';
+import { Observable } from 'rxjs';
+import { Task } from './../models/task';
 
 @Injectable()
 export class TaskService {
 
 constructor(private http: RequestService) { }
 
-public getAllTasks(boardId: string, columnId: string) {
+public getAllTasks(boardId: string, columnId: string): Observable<Task[]> {
   return this.http.getRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks')
 }
 
-public createTask(boardId: string, columnId: string, task: any) {
+public createTask(boardId: string, columnId: string, task: Task): Observable<Task> {
   return this.http.postRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks', task)
 }
 
-public getTask(boardId: string, columnId: string, taskId: string) {
+public getTask(boardId: string, columnId: string, taskId: string): Observable<Task> {
   return this.http.getRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks/' + taskId)
 }
 
-public deleteTask(boardId: string, columnId: string, taskId: string) {
+public deleteTask(boardId: string, columnId: string, taskId: string): Observable<Task> {
   return this.http.deleteRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks/' + taskId)
 }
 
-public updateTask(boardId: string, columnId: string, taskId: string, task: any) {
+public updateTask(boardId: string, columnId: string, taskId: string, task: Task): Observable<Task> {
   return this.http.putRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks/' + taskId, task)
 }
 }
