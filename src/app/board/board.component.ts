47913c03bb681970757f63f4cd5c13ea6f25ff56
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-25 20:31:35 +0200

    Add getBoard() method in routing

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BoardService } from './../services/board.service';
+import { map, switchMap, Subscription } from 'rxjs';
+import { Board } from '../models/board';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit, OnDestroy {
+
+  board!: Board;
+
+  subscribtion!: Subscription;
 
   constructor(private route: ActivatedRoute, private http: BoardService) { }
 
   ngOnInit() {
+    this.subscribtion = this.route.params
+    .pipe(
+      map(params => params['id']),
+      switchMap(boardId => this.http.getBoard(boardId))
+    )
+    .subscribe((board: Board) => this.board = board)
   }
-
+  
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe()
+  }
 }
