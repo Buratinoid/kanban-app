4b2afad8753a63544a447bc01a18a1e2dd02bcfd
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-25 13:32:55 +0200

    Add Observable<Board> and board: Board type

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Board } from './../models/board';
+import { Injectable, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RequestService } from './request.service';
 
@@ -7,23 +8,23 @@ export class BoardService {
 
 constructor(private http: RequestService) { }
 
-public getAllBoards(): Observable<any> {
-  return this.http.getRequest('/boards')
+public getAllBoards(): Observable<Board[]> {
+  return this.http.getRequest('/boards');
 }
 
-public createBoard(board: any): Observable<any> {
+public createBoard(board: Board): Observable<Board> {
   return this.http.postRequest('/boards', board)
 }
 
-public getBoard(boardId: string) {
+public getBoard(boardId: string): Observable<Board> {
   return this.http.getRequest('/boards/' + boardId)
 }
 
-public deleteBoard(boardId: string) {
+public deleteBoard(boardId: string): Observable<Board> {
   return this.http.deleteRequest('/boards/' + boardId)
 }
 
-public updateBoard(boardId: string, board: any) {
+public updateBoard(boardId: string, board: Board): Observable<Board> {
   return this.http.putRequest('/boards/' + boardId, board)
 }
 }
