63b9e984f9f601c49b1dbefcece5620da4e6ef5d
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-25 12:40:14 +0200

    Add getAllTasks() method

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,10 +1,12 @@
+import { RequestService } from './request.service';
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class TaskService {
 
-constructor() { }
+constructor(private http: RequestService) { }
 
+public getAllTasks(boardId: string, columnId: string) {
+  return this.http.getRequest('/boards/' + boardId + '/columns/' + columnId + '/tasks')
+}
 }
