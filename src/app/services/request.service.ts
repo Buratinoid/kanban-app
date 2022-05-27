2ca8d52f3edc4f5bfb2cac094932c775212d70e4
Roman <81187121+Buratinoid@users.noreply.github.com>
2022-05-27 11:45:53 +0200

    Add setter and getter for token

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -6,12 +6,19 @@ import { catchError, Observable, throwError, map } from 'rxjs';
 export class RequestService {
     
     private readonly url: string = 'http://localhost:8010/proxy';
-    private token: string = 'no token';
+    
+    private token$: string = 'no token';
+    private setToken(token: string): string {
+        return this.token$ = token;
+    }
+    private getToken(): string {
+        return this.token$;
+    }
     
     constructor(private request: HttpClient) { }
 
 public getRequest(path: string) {
-    const header = {'Authorization': 'Bearer ' + this.token}
+    const header = {'Authorization': 'Bearer ' + this.getToken()}
     return this.request.get<any>(this.url + path, { headers: header })
     .pipe(
         catchError(error => {
@@ -22,7 +29,7 @@ public getRequest(path: string) {
 }
 
 public postRequest(path: string, body: any) {
-    const header = {'Authorization': 'Bearer ' + this.token}
+    const header = {'Authorization': 'Bearer ' + this.getToken()}
     return this.request.post<any>(this.url + path, body, { headers: header })
     .pipe(
         catchError(error => {
@@ -33,7 +40,7 @@ public postRequest(path: string, body: any) {
 }
 
 public putRequest(path: string, body: any) {
-    const header = {'Authorization': 'Bearer ' + this.token}
+    const header = {'Authorization': 'Bearer ' + this.getToken()}
     return this.request.put<any>(this.url + path, body, { headers: header })
     .pipe(
         catchError(error => {
@@ -44,7 +51,7 @@ public putRequest(path: string, body: any) {
 }
 
 public deleteRequest(path: string) {
-    const header = {'Authorization': 'Bearer ' + this.token}
+    const header = {'Authorization': 'Bearer ' + this.getToken()}
     return this.request.delete<any>(this.url + path, { headers: header })
     .pipe(
         catchError(error => {
@@ -57,7 +64,7 @@ public deleteRequest(path: string) {
 public signInUser(body: any): Observable<any> {
     return this.postRequest('/signin', body)
     .pipe(
-        map(value => this.token = value.token),
+        map(value => this.setToken(value.token)),
         catchError(error => {
             console.log('Error: '+ error.error.statusCode + ' ' + error.error.message)
             return throwError(error)
@@ -66,10 +73,10 @@ public signInUser(body: any): Observable<any> {
 }
 
 public logOut() {
-    this.token = 'logout token';
+    this.setToken('no token');
 }
 
 public showToken() {
-    console.log(this.token);
+    console.log(this.getToken());
 }
 }
